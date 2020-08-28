53ae67051066d62f40b4b991d159f13ed794797b
sogb-gmao <70166308+sogb-gmao@users.noreply.github.com>
2020-08-28 11:08:32 +0000

    Add files via upload

---
+++ UnitTest/BusinessLogic.Test/DataConfigurationProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace BusinessLogic.Test
+{
+    /// <summary>
+    /// Configuration provider for data access.
+    /// </summary>
+    public class DataConfigurationProvider : Sinba.BusinessModel.ServiceInterface.IDataConfigurationProvider
+    {
+        /// <summary>
+        /// Gets the connection string.
+        /// </summary>
+        /// <value>
+        /// The connection string.
+        /// </value>
+        public string ConnectionString
+        {
+            get { return "SinbaContext"; }
+        }
+    }
+}
---
+++ UnitTest/BusinessLogic.Test/ExampleTests.cs
@@ -0,0 +1,29 @@
+﻿using Sinba.BusinessModel.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic.Test;
+using PostSharp.Constraints;
+
+namespace BusinessLogic.Tests
+{
+    /// <summary>
+    /// An example of test class.
+    /// </summary>
+    [TestClass()]
+    public class ExampleTests : TestBase
+    {
+        /// <summary>
+        /// A test method.
+        /// </summary>
+        [TestMethod()]
+        public void TestMethod()
+        {
+            // Implement your test here.
+        }
+    }
+
+    [Protected]
+    class TestAspect
+    {
+
+    }
+}
